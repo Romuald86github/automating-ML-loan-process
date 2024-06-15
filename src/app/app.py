ca8790160c49af39609ec4a76583d9413d93b01a
Romuald86github <123880565+Romuald86github@users.noreply.github.com>
2024-06-15 10:19:50 +0000

    updates

--- src/app/app.py
+++ src/app/app.py
@@ -7,11 +7,12 @@ def load_model():
     best_model = joblib.load('models/best_performing_model.joblib')
     preprocessing_pipeline = joblib.load('models/preprocessing_pipeline.joblib')
     selected_feature_names = joblib.load('models/selected_feature_names.joblib')
-    return best_model, preprocessing_pipeline, selected_feature_names
+    categorical_encoder = preprocessing_pipeline.named_steps['categorical_encoder']
+    return best_model, preprocessing_pipeline, selected_feature_names, categorical_encoder
 
 def run_streamlit_app():
     """Run the Streamlit web application for loan prediction."""
-    best_model, preprocessing_pipeline, selected_feature_names = load_model()
+    best_model, preprocessing_pipeline, selected_feature_names, categorical_encoder = load_model()
 
     st.set_page_config(page_title="Loan Prediction App", page_icon=":money_with_wings:")
     st.title("Loan Prediction App")
@@ -22,7 +23,7 @@ def run_streamlit_app():
         if feature_name in ['loan_amount', 'interest_rate', 'term', 'credit_score', 'annual_income']:
             input_data[feature_name] = st.number_input(f"{feature_name.replace('_', ' ').capitalize()}", min_value=0, step=1)
         else:
-            input_data[feature_name] = st.selectbox(f"{feature_name.replace('_', ' ').capitalize()}", options=preprocessing_pipeline.named_transformers_['categorical_encoder'].transformers_[0][1].categories_[selected_feature_names.index(feature_name)])
+            input_data[feature_name] = st.selectbox(f"{feature_name.replace('_', ' ').capitalize()}", options=categorical_encoder.categories_[selected_feature_names.index(feature_name)])
 
     # Create a DataFrame with the input features
     input_data = pd.DataFrame([input_data])
